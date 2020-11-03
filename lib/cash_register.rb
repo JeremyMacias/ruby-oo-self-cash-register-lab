eb0434c5fe5aaa3074a86cde7e5de9e57df31a68
Jeremy Macias <jeremypmacias@gmail.com>
2020-11-03 07:40:08 -0700

    commit tree d8dc25e609be7ac5335699b5de866e500d5f1000

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,50 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount, :items, :price, :quantity, :last_transaction
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(items, price, quantity = 1)
+        @price = price
+        @total += price * quantity
+        if quantity > 1
+            counter = 0
+            while counter < quantity
+                @items << items
+                counter += 1
+            end
+        else
+            @items << items
+        end
+    end
+
+    def apply_discount()
+        if @discount > 0
+            @discount = @discount/100.to_f
+            @total = @total - (@total * (@discount))
+            "After the discount, the total comes to $#{@total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+
+    end
+
+    
+    # @total -= @price   will leave just one failed test ('return total to 0.0')
+    def void_last_transaction
+        self.total -= self.price[-1]
+        if self.items == []
+            self.total = 0.0
+        end
+
+    end
+        
+
+
+
+end
